baa3b5303c2467aef9e5979205c0e6393c365456
Guilherme Almeida <guiallan.al@gmail.com>
2020-09-18 01:22:43 -0300

    feat(signout) create signout function but still no use

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -5,7 +5,7 @@ import { Form } from '@unform/web';
 import * as Yup from 'yup';
 import { Container, Content, Background } from './styles';
 
-import { useAuth } from '../../context/AuthContext';
+import { useAuth } from '../../hooks/AuthContext';
 import getValidationErrors from '../../utils/getValidationErrors';
 
 import Input from '../../components/Input';
